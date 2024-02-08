1081c32867cb7166adbf3f38d8c8265f6f83b0cf
inesgallo <anainesgf@gmail.com>
2024-02-08 11:41:04 +0100

    added home page

--- Ruleta/src/App.jsx
+++ Ruleta/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from "./Views/Login.jsx"
+/*import Login from "./Views/Login.jsx"*/
+import Home from "./Views/Home.jsx"
 
 function App() {
   
 
   return (
     <>
-     <Login/>
+     {/* <Login/> */}
+     <Home/>
     </>
   )
 }
