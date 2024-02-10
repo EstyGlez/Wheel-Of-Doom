c8b7669875791962459780338f9564f98cd77171
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2024-02-10 13:53:31 +0100

    responsive login

--- Ruleta/src/App.jsx
+++ Ruleta/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-/*import Login from "./Views/Login.jsx"*/
-import Welcome from "./Views/Welcome.jsx"
+import Login from "./Views/Login.jsx"
+// import Welcome from "./Views/Welcome.jsx"
 // import NavBar from "./Componentes/navBar/NavBar.jsx"
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
 
   return (
     <>
-     {/* <Login/> */}
-     <Welcome/>
+     <Login/>
+     {/* <Welcome/> */}
      {/* <NavBar/> */}
     </>
   )
