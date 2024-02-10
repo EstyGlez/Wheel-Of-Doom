7628eac420b0584ff1e78b5079a8221e7243998a
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2024-02-10 13:37:03 +0100

    welcome responsive

--- Ruleta/src/App.jsx
+++ Ruleta/src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 /*import Login from "./Views/Login.jsx"*/
-// import Welcome from "./Views/Welcome.jsx"
-import NavBar from "./Componentes/navBar/NavBar.jsx"
+import Welcome from "./Views/Welcome.jsx"
+// import NavBar from "./Componentes/navBar/NavBar.jsx"
 
 function App() {
   
@@ -12,8 +12,8 @@ function App() {
   return (
     <>
      {/* <Login/> */}
-     {/* <Welcome/> */}
-     <NavBar/>
+     <Welcome/>
+     {/* <NavBar/> */}
     </>
   )
 }
