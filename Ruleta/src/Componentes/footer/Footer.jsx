555ed113b415f2099929e1d886796c53fc4a733d
EstyGlez <estibalizzmakeup@gmail.com>
2024-02-13 09:53:21 +0100

    Footer Added

---
+++ Ruleta/src/Componentes/footer/Footer.jsx
@@ -0,0 +1,22 @@
+//import React from 'react'
+import Queen from "../BlogQueen/Queen.jsx";
+import "./footer.css";
+import la_reina from "./images_footer/la_reina.svg";
+
+function Footer() {
+
+    return (
+        <>
+            <footer className='footer'>
+                <section>
+                    <img src={la_reina} alt='reina_cotilla' />
+                   
+                    <Queen />
+                    <h1>&copy; Developed by Grupo 4 Creations © </h1>
+                </section>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
